8e60b1ddfbabbc93349111b06b04962e6b884fec
city-map-stade <m.seir@city-map.de>
2021-10-15 09:25:52 +0200

    file added

---
+++ city-map-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: city-map Installer 
+Plugin URI: https://www.internet-erfolg.de
+Description: Fastinstaller for plugins, required by city-map
+Version: 1.0
+Author: Maurice Pauluhn, city-map Stade GmbH
+Author URI: https://www.internet-erfolg.de
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
